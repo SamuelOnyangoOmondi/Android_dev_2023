ecd1f5f56ca38eebb8ca849d728ad266f0cc48d1
Martin <gichurindungu@gmail.com>
2023-12-03 23:12:39 +0300

    refine tests

--- test/loginpage_test.dart
+++ test/loginpage_test.dart
@@ -7,42 +7,38 @@ import 'package:flutter_app/login/login.dart';
 
 void main() {
   testWidgets('LoginPage widget test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(
       home: LoginPage(
-        onTap: () {}, // Provide a mock onTap function
+        onTap: () {},
       ),
     ));
-
-    // Verify that the logo is displayed
+    // lock display test
     expect(find.byIcon(Icons.lock), findsOneWidget);
 
-    // Verify that the welcome text is displayed
+    // welcome text test
     expect(find.text('Welcome back you\'ve been missed!'), findsOneWidget);
 
-    // Verify that the email and password text fields are displayed
+    // test for the email and password widgets
     expect(find.byType(MyTextField), findsNWidgets(2));
 
-    // Verify that the "Forgot Password?" text is displayed
+    // test for the forgot password text
     expect(find.text('Forgot Password?'), findsOneWidget);
 
-    // Verify that the sign-in button is displayed
+    // test sign-in button display
     expect(find.byType(MyButton), findsOneWidget);
 
-    // Verify that the "Or continue with" text is displayed
+    // continue with text test
     expect(find.text('Or continue with'), findsOneWidget);
 
-    // Verify that the Google and Apple sign-in buttons are displayed
+    // test google and apple widgets
     expect(find.byType(SquareTile), findsNWidgets(2));
 
-    // Verify that the "Not a member?" text is displayed
+    // test new members
     expect(find.text('Not a member?'), findsOneWidget);
 
-    // Verify that the "Register now" text is displayed and tap triggers onTap
+    // new registration nav/widget
     expect(find.text('Register now'), findsOneWidget);
     await tester.tap(find.text('Register now'));
     await tester.pump();
-
-    // Add more tests as needed
   });
 }
--- test/register_test.dart
+++ test/register_test.dart
@@ -10,37 +10,26 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MaterialApp(
       home: RegisterPage(
-        onTap: () {}, // Provide a mock onTap function
+        onTap: () {},
       ),
     ));
 
-    // Verify that the logo is displayed
     expect(find.byIcon(Icons.lock), findsOneWidget);
 
-    // Verify that the welcome text is displayed
     expect(find.text('Welcome to our super app!'), findsOneWidget);
 
-    // Verify that the email, password, and confirm password text fields are displayed
     expect(find.byType(MyTextField), findsNWidgets(3));
 
-    // Verify that the "Forgot Password?" text is displayed
     expect(find.text('Forgot Password?'), findsOneWidget);
 
-    // Verify that the sign-up button is displayed
     expect(find.byType(MyButton), findsOneWidget);
 
-    // Verify that the "Or continue with" text is displayed
     expect(find.text('Or continue with'), findsOneWidget);
 
-    // Verify that the Google and Apple sign-in buttons are displayed
     expect(find.byType(SquareTile), findsNWidgets(2));
 
-    // Verify that the "Already have an account?" text is displayed
     expect(find.text('Already have an account?'), findsOneWidget);
 
-    // Verify that the "Login now" text is displayed
     expect(find.text('Login now'), findsOneWidget);
   });
-
-  // Add more tests as needed
 }
